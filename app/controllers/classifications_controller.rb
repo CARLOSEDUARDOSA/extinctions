b8241676438bf2b3c3b20dc9545c36003647a2a7
evelynbernardo <bernardoevelynbarros@gmail.com>
2015-02-10 23:20:41 +0000

    Added Animals controller and views

---
+++ app/controllers/classifications_controller.rb
@@ -0,0 +1,9 @@
+class ClassificationsController < ApplicationController
+	def index
+			@Classifications = Classification.all
+	end
+
+	def show
+			@Classification = Classification.find(params[:id])
+	end
+end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,7 +1,7 @@
 class Animal < ActiveRecord::Base
   belongs_to :classification
   has_many :common_names
-  def extinctionDate?
+  def recent?
 	if extinction_date > 1900
 		return true
 	else
